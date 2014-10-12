f497ee74c38a4b2aa5e70ff28170e9f43a2e9b44
Skander Kort <skanderkort@gmail.com>
2014-10-12 14:41:33 -0400

    Features: add flight, remove flight and rename trip

--- src/KortS/TripBundle/Controller/TripController.php
+++ src/KortS/TripBundle/Controller/TripController.php
@@ -5,9 +5,10 @@ namespace KortS\TripBundle\Controller;
 use \Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-
 class TripController extends BaseController
 {
+    
+    const MAX_TRIP_NAME_LENGTH = 128;
 
     /**
      * Returns all the flights of trip $tripId.
@@ -17,47 +18,31 @@ class TripController extends BaseController
     public function flightsAction($tripId)
     {
         try {
-            $this->assertIsNonNegativeInteger($tripId, 'Trip Id has to be non negative');
-            $flights = $this->findFlights($tripId);
-            
+            $this->assertIsPositiveInteger($tripId, 'Trip Id');
+            $flights = $this->findTrip($tripId)->getFlights();
+
             return $this->flightsActionResponse($tripId, $flights);
-            
         } catch (\Exception $e) {
-             return $this->buildErrorResponse($e);
+            return $this->buildErrorResponse($e);
         }
     }
-    
+
     /**
-     * Checks if $value is a non negative integer.
-     * 
-     * @param integer $value
-     * @param string $errorMessage
-     * @throws \InvalidArgumenException
-     */
-    private function assertIsNonNegativeInteger($value, $errorMessage)
-    {
-        if (false == ctype_digit($value)) {
-            throw new \InvalidArgumenException($errorMessage);
-        }
-    }
-    
-    /**
-     * Returns the flights of a trip. Throws an InvalidArgumentException when the trip
-     * cannot be found.
+     * Returns  a trip. Throws an InvalidArgumentException when the trip does not exist.
      * 
      * @param integer $tripId
-     * @return \Doctrine\Common\Collections\ArrayCollection
+     * @return KortSTripBundle\Entity\Trip
      * @throws \InvalidArgumenException
      */
-    private function findFlights($tripId)
+    private function findTrip($tripId)
     {
-         $trip = $this->get('doctrine')->getManager()->find('KortSTripBundle:Trip', $tripId);
-         
+        $trip = $this->getDoctrine()->getManager()->find('KortSTripBundle:Trip', $tripId);
+
         if (empty($trip)) {
             throw new \InvalidArgumenException('Trip not found');
         }
-        
-        return $trip->getFlights();
+
+        return $trip;
     }
 
     /**
@@ -70,32 +55,203 @@ class TripController extends BaseController
      */
     private function flightsActionResponse($tripId, ArrayCollection $flights)
     {
-         $response = array(
-             'status' => 'OK',
-             'tripId'   => $tripId,
-             'flights'  => array()
+        $response = array(
+            'status'  => 'OK',
+            'tripId'  => $tripId,
+            'flights' => array()
+        );
+
+        foreach ($flights as $flight) {
+            $response['flights'][] = $flight->toArray();
+        }
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * Adds a new flight to a trip.
+     * 
+     * @param integer $tripId
+     * @param integer $departureAirportId
+     * @param integer $arrivalAirportId
+     */
+    public function addFlightAction($tripId, $departureAirportId, $arrivalAirportId)
+    {
+        try {
+            $this->assertIsPositiveInteger($tripId, 'Trip Id');
+            $this->assertIsPositiveInteger($departureAirportId, 'Departure airport id');
+            $this->assertIsPositiveInteger($arrivalAirportId, 'Arrival airport id');
+
+            $trip = $this->findTrip($tripId);
+            $newFlight = $this->findFlightByAirports($departureAirportId, $arrivalAirportId);
+
+            $trip->addFlight($newFlight);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->addFlightResponse($tripId, $departureAirportId, $arrivalAirportId);
+        } catch (\Exception $e) {
+            return $this->buildErrorResponse($e);
+        }
+    }
+
+    /**
+     * Returns  a flight from a given airport to some other airport. 
+     * Throws an InvalidArgumentException when the flight does not exist.
+     * 
+     * @param integer $departureAirportId
+     * @param integer $arrivalAirportId
+     * @return KortSTripBundle\Entity\Flight
+     * @throws \InvalidArgumenException
+     */
+    private function findFlightByAirports($departureAirportId, $arrivalAirportId)
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository('KortSTripBundle:Flight');
+
+        $flights = $repository->findBy(array(
+            'departureAirport' => $departureAirportId,
+            'arrivalAirport'   => $arrivalAirportId
+            )
+        );
+
+        if (empty($flights)) {
+            throw new \InvalidArgumentException(sprintf("There are no direct flights from airport %d to airport %d", $departureAirportId, $arrivalAirportId
+                )
             );
-         
-         foreach($flights as $flight) {
-             $response['flights'][] = $flight->toArray();
-         }
-         
-         return new JsonResponse($response);
+        }
+
+        // Since Trip builder application does not handle departure and arrival times, there 
+        // will be at most one flight from one airport to another.
+        return $flights[0];
     }
 
-    public function addFlightAction($departureAirportId, $arrivalAirportId)
+    /**
+     * 
+     * @param integer $tripId
+     * @param integer $departureAirportId
+     * @param integer $arrivalAirportId
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    private function addFlightResponse($tripId, $departureAirportId, $arrivalAirportId)
     {
-        
+        $response = array(
+            'status'             => 'OK',
+            'tripId'             => $tripId,
+            'departureAirportId' => $departureAirportId,
+            'arrivalAirportId'   => $arrivalAirportId
+        );
+
+        return new JsonResponse($response);
     }
 
-    public function removeFlightAction($flightId)
+    /**
+     * Removes a flight from a trip. 
+     * Trying to remove a flight that is not part of the trip does not lead to an error.
+     * 
+     * @param integer $tripId
+     * @param integer $flightId
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function removeFlightAction($tripId, $flightId)
     {
-        
+        try {
+            $this->assertIsPositiveInteger($tripId, 'Trip Id');
+            $this->assertIsPositiveInteger($flightId, 'Flight Id');
+
+            $trip = $this->findTrip($tripId);
+            $flight = $this->findFlight($flightId);
+            $trip->removeFlight($flight);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->removeFlightResponse($tripId, $flightId);
+        } catch (\Exception $e) {
+            return $this->buildErrorResponse($e);
+        }
     }
 
-    public function renameAction($tripId)
+    /**
+     * Returns  a flight. Throws an InvalidArgumentException when the flight does not exist.
+     * 
+     * @param integer $tripId
+     * @return KortSTripBundle\Entity\Flight
+     * @throws \InvalidArgumenException
+     */
+    private function findFlight($flightId)
     {
-        
+        $flight = $this->getDoctrine()->getManager()->find('KortSTripBundle:Flight', $flightId);
+
+        if (empty($flight)) {
+            throw new \InvalidArgumenException('Flight not found');
+        }
+
+        return $flight;
+    }
+
+    /**
+     * 
+     * @param integer $tripId
+     * @param integer $flightId
+     */
+    private function removeFlightResponse($tripId, $flightId)
+    {
+        $response = array(
+            'status'   => 'OK',
+            'tripId'   => $tripId,
+            'flightId' => $flightId,
+        );
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * Updates the name of a given trip
+     * 
+     * @param integer $tripId
+     * @param string $name
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function renameAction($tripId, $name)
+    {
+        try {
+            $this->assertIsPositiveInteger($tripId, 'Trip Id');
+            $this->assertIsValidTripName($name);
+            $this->findTrip($tripId)->setName();
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->renameActionResponse($tripId, $name);
+        } catch (\Exception $e) {
+            return $this->buildErrorResponse($e);
+        }
+    }
+    
+    /**
+     * 
+     * Asserts that trip name has a valid length.
+     * 
+     * @param string $name
+     * @throws \InvalidArgumentException
+     */
+    private function assertIsValidTripName($name)
+    {
+        $l = strlen($name);
+        if ($l == 0 || $l > self::MAX_TRIP_NAME_LENGTH) {
+            throw new \InvalidArgumentException('Invalid trip name');
+        }
+    }
+
+    /**
+     * 
+     * @param integer $tripId
+     * @param string $name
+     */
+    private function renameActionResponse($tripId, $name)
+    {
+        $response = array(
+            'status'  => 'OK',
+            'tripId'  => $tripId,
+            'newName' => $name,
+        );
+
+        return new JsonResponse($response);
     }
 
 }
